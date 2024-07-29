f86d9cf26ae6ed0028e075ff38f295f40bd5dd3b
Kyler Kramer <kyler@Kyler.Kylertk21>
2024-07-29 15:02:02 -0600

    contacts can now be added to DB through GUI

--- src/model.py
+++ src/model.py
@@ -5,6 +5,7 @@
 
 from PyQt6.QtCore import Qt
 from PyQt6.QtSql import QSqlTableModel as qtmodel
+from PyQt6.QtWidgets import QMessageBox as msbox
 
 class ContactsModel:
     def __init__(self):
@@ -14,9 +15,21 @@ class ContactsModel:
     def _createModel():
         tableModel = qtmodel()
         tableModel.setTable("contacts")
-        tableModel.setEditStrategy(qtmodel.EditStrategy.OnFieldChange)
+        tableModel.setEditStrategy(qtmodel.EditStrategy.OnManualSubmit)
         tableModel.select()
         headers = ("ID", "Name", "Phone", "Email")
         for columnIndex, header in enumerate(headers):
             tableModel.setHeaderData(columnIndex, Qt.Orientation.Horizontal, header)
         return tableModel
+    
+    def addContact(self,data):
+        """Add a contact to the database"""
+        rows = self.model.rowCount()
+        self.model.insertRows(rows,1)
+        self.model.setData(self.model.index(rows, 1), data[0])
+        self.model.setData(self.model.index(rows, 2), data[1])
+        self.model.setData(self.model.index(rows, 3), data[2])
+        if not self.model.submitAll():
+            msbox.critical(None, "Database Error", 
+                           f"Error submitting data: {self.model.lastError().text()}")
+        self.model.select()
--- src/views.py
+++ src/views.py
@@ -10,8 +10,15 @@ from PyQt6.QtWidgets import (
     QTableView as tview,
     QVBoxLayout as vbox,
     QWidget as widg,
+    QDialog,
+    QDialogButtonBox as bbox,
+    QFormLayout as flayout,
+    QLineEdit as ledit,
+    QMessageBox as msbox
 )
+from PyQt6.QtCore import Qt
 from .model import ContactsModel
+from .database import createConnection
 
 class Window(win):
     """Main Window"""
@@ -24,6 +31,11 @@ class Window(win):
         self.setCentralWidget(self.centralWidget)
         self.layout = qbox()
         self.centralWidget.setLayout(self.layout)
+        
+        if not createConnection("contacts"):
+            msbox.critical(None, 'Database Connection', 'Database connection failed!')
+            exit(1)
+        
         self.contactsModel = ContactsModel()
         self.setupUI()
     
@@ -34,6 +46,7 @@ class Window(win):
         self.table.setSelectionBehavior(absview.SelectionBehavior.SelectRows)
         
         self.addBtn = pushbtn("Add...")
+        self.addBtn.clicked.connect(self.openAddDialog)
         self.delBtn = pushbtn("Delete")
         self.clrAll = pushbtn("Clear All")
 
@@ -46,5 +59,64 @@ class Window(win):
         self.layout.addLayout(layout)
 
         self.table
+    
+    def openAddDialog(self):
+        dialog = AddDialog(self)
+        if dialog.exec() == QDialog.DialogCode.Accepted:
+            self.contactsModel.addContact(dialog.data)
+            self.table.resizeColumnsToContents()
+
+class AddDialog(QDialog):
+    """Add Contact Dialog Box"""
+    def __init__(self, parent=None):
+        """Initializer"""
+        super().__init__(parent=parent)
+        self.setWindowTitle("Add Contact")
+        self.layout = vbox()
+        self.setLayout(self.layout)
+        self.data = None
+        
+        self.setupUI()
+    
+    def setupUI(self):
+        """Setup Add Contact Dialog's GUI"""
+        self.nameField = ledit()
+        self.nameField.setObjectName("Name")
+        self.phoneField = ledit()
+        self.phoneField.setObjectName("Phone")
+        self.emailField = ledit()
+        self.emailField.setObjectName("Email")
+
+        layout = flayout()
+        layout.addRow("Name:", self.nameField)
+        layout.addRow("Phone:", self.phoneField)
+        layout.addRow("Email:", self.emailField)
+        self.layout.addLayout(layout)
+
+        self.buttonsBox = bbox(self)
+        self.buttonsBox.setOrientation(Qt.Orientation.Horizontal)
+        self.buttonsBox.setStandardButtons(
+            bbox.StandardButton.Ok | bbox.StandardButton.Cancel
+        )
+        self.buttonsBox.accepted.connect(self.accept)
+        self.buttonsBox.rejected.connect(self.reject)
+        self.layout.addWidget(self.buttonsBox)
+
+    def accept(self):
+        # TODO: add more robust validations rules
+        """Accept data from dialog box"""
+        self.data = []
+        for field in (self.nameField, self.phoneField, self.emailField):
+            if not field.text():
+                msbox.critical(
+                    self,
+                    "Error!",
+                    f"You must provide a contact's {field.objectName()}",
+                )
+                self.data = None
+                return
+            self.data.append(field.text())
+
+        super().accept()
 
 
